681fb19dabe4ad5689e154c304e735a02a2dc706
sriram-24 <sriram1999m@gmail.com>
2024-12-28 21:25:41 +0530

    fix:ts import to js

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,21 @@
-import express,{ Express, Request, Response } from "express";
-import {db} from './db/db.ts' 
-import 'dotenv/config';
+import express, { Express, Request, Response } from "express";
+import { db } from "./db/db";
+import "dotenv/config";
 
-const app : Express = express();
- 
-app.get("/", async(_req : Request, res : Response)=>{
+const app: Express = express();
+
+app.get("/", async (_req: Request, res: Response) => {
   try {
-   const result = await db.query("SELECT * FROM USERS");
-  res.json(result.rows);
+    const result = await db.query("SELECT * FROM USERS");
+    res.json(result.rows);
   } catch (error) {
-    console.log("Error occured",error);
-    res.status(500).send("Internal Server Error")
+    console.log("Error occured", error);
+    res.status(500).send("Internal Server Error");
   }
-})
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-console.log("Server started on port",PORT);
-})
+app.listen(PORT, () => {
+  console.log("Server started on port", PORT);
+});
