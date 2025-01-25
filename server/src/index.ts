8280fa54d3fc4572802a7f831d1036b598999f49
sriram-24 <sriram1999m@gmail.com>
2025-01-25 18:53:19 +0530

    feat: next js template added.

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,17 +5,18 @@ import "dotenv/config";
 const app: Express = express();
 
 app.get("/", async (_req: Request, res: Response) => {
-  try {
-    const result = await db.query("SELECT * FROM USERS");
-    res.json(result.rows);
-  } catch (error) {
-    console.log("Error occured", error);
-    res.status(500).send("Internal Server Error");
-  }
+    try {
+        const result = await db.query("SELECT * FROM USERS");
+        res.json(result.rows);
+    } catch (error) {
+        console.log("Error occured", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server started on port", PORT);
+    console.log("Server started on port", PORT);
 });
+
