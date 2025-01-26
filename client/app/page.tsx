33937856d4eaba71fa77b57a3cb2232d9eab72b6
sriram-24 <sriram1999m@gmail.com>
2025-01-26 19:41:26 +0530

    fix: cors issue in next config.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [users,setUsers] = useState([]);
+    useEffect(()=>{
+        async function getUsers(){
+            const res = await fetch("http://localhost:3000/");
+            const data = await res.json();
+            if(data) {
+                setUsers(data)
+            }
+        }
+        getUsers()
+    },[])
     return (
         <div>
-            hello
+            {users && users.map((obj, index) => <div key={obj.id}>{obj.name}</div>)}
         </div>
     );
 }
--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async rewrites() {
+    return [
+    {
+        source: '/:path*',  // Match any path
+        destination: 'http://localhost:3000/:path*',
+    },
+    ];
+  },
 };
 
 export default nextConfig;
