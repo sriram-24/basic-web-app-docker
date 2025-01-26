b85ece656cdb8c91804041f879e6cd8b5a86acb7
sriram-24 <sriram1999m@gmail.com>
2025-01-26 19:40:59 +0530

    fix: cors issue.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,13 @@
 import express, { Express, Request, Response } from "express";
 import { db } from "./db/db";
 import "dotenv/config";
-
+const cors = require('cors');
 const app: Express = express();
+
+app.use(cors({
+  origin: 'http://localhost:3001',  // Allow your Next.js client
+  methods: ['GET', 'POST'], // Specify the allowed HTTP methods
+}));
 
 app.get("/", async (_req: Request, res: Response) => {
     try {
