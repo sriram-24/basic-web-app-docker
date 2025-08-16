347d60359592f5588398f57a3deb7bd07bcca33b
sriram-24 <sriram1999m@gmail.com>
2025-08-16 19:16:16 +0530

    chore: moved api call to separate route(/users).

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,22 +1,14 @@
 "use client";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+
+export interface User{
+    id:string
+    name:string
+}
 
 export default function Home() {
-    const [users,setUsers] = useState([]);
-    useEffect(()=>{ 
-        async function getUsers(){ 
-            const res = await fetch(process.env.NEXT_PUBLIC_SERVER_HOST);
-            const data = await res.json();
-            if(data) {
-                setUsers(data)
-            }
-        }
-        getUsers()
-    },[])
-    return (
+       return (
         <div>
-            {users && users.map((obj, index) => <div key={obj.id}>{obj.name}</div>)}
+            <a href="/users/">open users</a>
         </div>
     );
 }
