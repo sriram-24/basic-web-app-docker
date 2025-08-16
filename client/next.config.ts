46449fee1ab829915eba9d0729525254dd8c6a5a
sriram-24 <sriram1999m@gmail.com>
2025-08-16 19:17:59 +0530

    chore: updated standalone deployment option.

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    distDir: 'build',
+    output: 'standalone',
     async rewrites() {
     return [
     {
         source: '/:path*',  // Match any path
-        destination: 'http://localhost:8080/:path*',
-        
+        destination: 'http://localhost:8080/:path*', 
     },
     ];
   },
